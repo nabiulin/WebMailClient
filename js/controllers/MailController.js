db79e0074c5c9696c10265b5bccdfd40d131b17d
Sergey Nabiulin <nabiulin@nixsolutions.com>
2012-11-01 13:40:37 +0200

    Added option buttons

--- js/controllers/MailController.js
+++ js/controllers/MailController.js
@@ -11,11 +11,27 @@ function MailController($scope, $routeParams) {
     $scope.mails = self.mails;
 
     /**
-     * Mark all letters as read
+     * Mark all mails as read
      */
     $scope.markRead = function() {
         angular.forEach(self.mails, function(value){
-            value.isNew = 0;
+            if (value.isNew) {
+                value.isNew = 0;
+            }
         })
+    };
+
+    /**
+     * Remove checked mails
+     */
+    $scope.remove = function() {
+        var elements = $('#mails').find('input[type="checkbox"]:checked');
+        angular.forEach(elements, function(element){
+            angular.forEach(self.mails, function(mail){
+                if ($(element).data('id') == mail.id) {
+                    self.mails.splice(self.mails.indexOf(mail), 1);
+                }
+            })
+        });
     };
 }
